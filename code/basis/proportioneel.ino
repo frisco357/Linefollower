315d22693ed0f77231390d78789df1d85b3be220
frisco357 <59982448+frisco357@users.noreply.github.com>
2023-11-10 02:17:54 +0100

    Add files via upload

---
+++ code/basis/proportioneel.ino
@@ -0,0 +1,235 @@
+#include "SerialCommand.h"
+#include "EEPROMAnything.h"
+
+#define SerialPort Serial1
+#define Baudrate 9600
+
+#define MotorLeftForward 6
+#define MotorLeftBackward 9
+#define MotorRightForward 5
+#define MotorRightBackward 3
+
+SerialCommand sCmd(SerialPort);
+const uint8_t btn = 1;
+uint8_t btn_prev = HIGH;
+bool debug;
+bool run;
+unsigned long previous, calculationTime;
+
+const int sensor[] = {A0, A1, A2, A3, A4, A5};
+struct param_t
+{
+  unsigned long cycleTime;
+  int black[6];
+  int white[6];
+  int power;
+  float diff;
+  float kp;
+} params;
+
+int normalised[6];
+float debugPosition;
+
+
+
+void setup()
+{
+  run = false;
+  SerialPort.begin(Baudrate);
+  pinMode(13, OUTPUT);
+  pinMode(2, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(2), StartStop, FALLING);
+  
+  sCmd.addCommand("set", onSet);
+  sCmd.addCommand("debug", onDebug);
+  sCmd.addCommand("calibrate", onCalibrate);
+  sCmd.addCommand("run", onRun);
+  sCmd.addCommand("stop", onStop);
+  sCmd.setDefaultHandler(onUnknownCommand);
+
+
+  EEPROM_readAnything(0, params);
+  
+  SerialPort.println("ready");
+}
+
+void loop()
+{
+  sCmd.readSerial();
+  digitalWrite(13, HIGH);
+  unsigned long current = micros();
+  if (current - previous >= params.cycleTime)
+  {
+    previous = current;
+    
+    for (int i= 0; i< 6; i++)
+    {
+      normalised[i] = map(analogRead(sensor[i]), params.black[i], params.white[i], 0, 1000);
+    }
+
+    int index = 0;
+    float position = 0;
+    for (int i = 1; i < 6; i++) if (normalised[i] < normalised[index]) index = i;
+
+    if (normalised[index] > 800) run = false;
+    
+    
+    if (index == 0) position = -23.8;
+    else if (index == 5) position = 23.8;
+    else
+    {
+      int sNul = normalised[index];
+      int sMinEen = normalised[index-1];
+      int sPlusEen = normalised[index+1];
+
+      float b = sPlusEen - sMinEen;
+      b = b / 2;
+
+      float a = sPlusEen - b - sNul;
+
+      float position = -b /(2 * a);
+      position += index;
+      position -= 2.5;
+
+      position *= 9.5;
+    }
+    debugPosition = position;
+
+    float error = -position;
+    float output = error * params.kp;
+    
+    output = constrain(output, -510, 510);
+
+    int powerLeft = 0;
+    int powerRight = 0;
+
+    if (run) if (output >= 0)
+    {
+      powerLeft = constrain(params.power + params.diff * output, -255, 255);
+      powerRight = constrain(powerLeft - output, -255, 255);
+      powerLeft = powerRight + output;
+    }
+    else
+    {
+      powerRight = constrain(params.power - params.diff * output, -255, 255);
+      powerLeft = constrain(powerRight + output, -255, 255);
+      powerRight = powerLeft - output;
+    }
+
+    analogWrite(MotorLeftForward, powerLeft > 0 ? powerLeft : 0);
+    analogWrite(MotorLeftBackward, powerLeft < 0 ? -powerLeft : 0);
+    analogWrite(MotorRightForward, powerRight > 0 ? powerRight : 0);
+    analogWrite(MotorRightBackward, powerRight < 0 ? -powerRight : 0);
+    /* pid regeling */
+
+    /* aansturen motoren */
+  }
+
+  unsigned long difference = micros() - current;
+  if (difference > calculationTime) calculationTime = difference;
+}
+
+
+void onUnknownCommand(char *command)
+{
+  SerialPort.print("unknown command: \"");
+  SerialPort.print(command);
+  SerialPort.println("\"");
+}
+
+void onSet()
+{
+  char* param = sCmd.next();
+  char* value = sCmd.next();  
+  
+  if (strcmp(param, "cycle") == 0) params.cycleTime = atol(value);
+  else if (strcmp(param, "power") == 0) params.power = atol(value);
+  else if (strcmp(param, "diff") == 0) params.diff = atof(value);
+  else if (strcmp(param, "kp") == 0) params.kp = atof(value);
+  
+  EEPROM_writeAnything(0, params);
+}
+
+void onDebug()
+{
+  SerialPort.print("cycle time: ");
+  SerialPort.println(params.cycleTime);
+  
+  SerialPort.print("black: ");
+  for (int i = 0; i < 6; i++)
+  {
+    SerialPort.print(params.black[i]);
+    SerialPort.print(" ");
+  }
+  SerialPort.println(" ");
+
+  SerialPort.print("white: ");
+  for (int i = 0; i < 6; i++)
+  {
+    SerialPort.print(params.white[i]);
+    SerialPort.print(" ");
+  }
+  SerialPort.println(" ");
+
+  SerialPort.print("normalised: ");
+  for (int i =0; i < 6; i++)
+  {
+    SerialPort.print(normalised[i]);
+    SerialPort.print(" ");
+  }
+  SerialPort.println(" ");
+
+  SerialPort.print("position: ");
+  SerialPort.println(debugPosition);
+
+  SerialPort.print("power: ");
+  SerialPort.println(params.power);
+  SerialPort.print("diff: ");
+  SerialPort.println(params.diff);
+  SerialPort.print("kp: ");
+  SerialPort.println(params.kp);
+  
+  SerialPort.print("calculation time: ");
+  SerialPort.println(calculationTime);
+  calculationTime = 0;
+}
+
+void onCalibrate()
+{
+  char* param = sCmd.next();
+
+  if (strcmp(param, "black") == 0)
+  {
+    SerialPort.print("start calibrating black... ");
+    for (int i = 0; i < 6; i++) params.black[i]=analogRead(sensor[i]);
+    SerialPort.println("done");
+  }
+  else if (strcmp(param, "white") == 0)
+  {
+    SerialPort.print("start calibrating white... ");    
+    for (int i = 0; i < 6; i++) params.white[i]=analogRead(sensor[i]);  
+    SerialPort.println("done");      
+  }
+
+  EEPROM_writeAnything(0, params);
+}
+
+void onRun()
+{
+ run = true;  
+}
+
+void onStop()
+{
+  run = false;
+}
+
+void StartStop()
+{
+  if (run)
+  {
+    run = false;
+  }
+  else run = true;
+  SerialPort.println("interrupt");
+}
